f8e9a6770006fb9a004751080ebcac101b9be99b
Adeonir Kohl <adeonir@gmail.com>
2024-03-16 20:26:15 -0300

    Add checkout to express routes

--- src/index.ts
+++ src/index.ts
@@ -2,6 +2,7 @@ import express from 'express'
 
 import { createUser, findUserById, getUsers } from '~/controllers/user'
 
+import { createCheckout } from './controllers/subscription'
 import { createTask, getTasks } from './controllers/task'
 
 const app = express()
@@ -10,10 +11,13 @@ const port = 3000
 app.use(express.json())
 
 app.get('/users', getUsers)
-app.post('/users', createUser)
 app.get('/users/:id', findUserById)
+app.post('/users', createUser)
+
 app.get('/tasks', getTasks)
 app.post('/tasks', createTask)
+
+app.get('/checkout', createCheckout)
 
 app.listen(port, () => {
   console.info(`Server is running on port ${port}`)
