1684710ccc00cc93c35ae43cf0ad1aefb9581023
Adeonir Kohl <adeonir@gmail.com>
2024-03-16 13:24:17 -0300

    Add pro plan price id from stripe

--- src/utils/env.ts
+++ src/utils/env.ts
@@ -3,6 +3,7 @@ import { z } from 'zod'
 export const envSchema = z.object({
   STRIPE_PUBLISHABLE_KEY: z.string(),
   STRIPE_SECRET_KEY: z.string(),
+  PRO_PLAN_PRICE_ID: z.string(),
 })
 
 export type EnvSchemaType = z.infer<typeof envSchema>
