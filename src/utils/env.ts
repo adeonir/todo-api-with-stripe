086e34dd4c22e516662a48cc00d7aceb07608901
Adeonir Kohl <adeonir@gmail.com>
2024-03-16 13:09:51 -0300

    Create env validations

---
+++ src/utils/env.ts
@@ -0,0 +1,17 @@
+import { z } from 'zod'
+
+export const envSchema = z.object({
+  STRIPE_PUBLISHABLE_KEY: z.string(),
+  STRIPE_SECRET_KEY: z.string(),
+})
+
+export type EnvSchemaType = z.infer<typeof envSchema>
+
+const parsed = envSchema.safeParse(process.env)
+
+if (!parsed.success) {
+  console.error('Invalid environment variables.', parsed.error.format())
+  throw new Error(`Invalid environment variables.\n${JSON.stringify(parsed.error.format(), null, 2)}`)
+}
+
+export const env = parsed.data
