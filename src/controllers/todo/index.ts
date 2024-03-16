2641655013f215313beb855b53d1594ff509676d
Adeonir Kohl <adeonir@gmail.com>
2024-03-16 12:34:22 -0300

    Create function with validations to create new task

---
+++ src/controllers/todo/index.ts
@@ -0,0 +1,47 @@
+import type { Request, Response } from 'express'
+
+import { prisma } from "~/lib/prisma"
+import type { CreateTaskInput } from "./types"
+import { createTaskSchema } from "./types"
+import { z } from "zod"
+
+export const createTask = async (request: Request, response: Response) => {
+  const userId = request.headers['x-user-id'] as string
+
+  if (!userId) {
+    return response.status(401).send({ error: 'Unauthorized' })
+  }
+
+  const user = await prisma.user.findUnique({ where: { id: userId } })
+
+  if (!user) {
+    return response.status(401).send({ error: 'Unauthorized' })
+  }
+
+  let parsed: CreateTaskInput
+
+  try {
+    parsed = createTaskSchema.parse(request.body)
+  } catch (error) {
+    if (error instanceof z.ZodError) {
+      const errorMessage = error.errors.map(e => e.message).join(', ');
+      return response.status(400).send({ error: errorMessage });
+    }
+    return response.status(500).send({ error: 'Internal server error' })
+  }
+
+  const { title } = parsed
+
+  const task = await prisma.task.create({
+    data: {
+      title,
+      user: {
+        connect: {
+          id: userId
+        }
+      }
+    }
+  })
+
+  return response.status(201).send(task)
+}
--- src/index.ts
+++ src/index.ts
@@ -1,5 +1,6 @@
 import express from 'express'
 import { createUser, findUserById, listUsers } from "~/controllers/user"
+import { createTask } from "./controllers/todo"
 
 const app = express()
 const port = 3000
@@ -9,6 +10,7 @@ app.use(express.json())
 app.get('/users', listUsers)
 app.post('/users', createUser)
 app.get('/users/:id', findUserById)
+app.post('/tasks', createTask)
 
 app.listen(port, () => {
   console.log(`Server is running on port ${port}`)
