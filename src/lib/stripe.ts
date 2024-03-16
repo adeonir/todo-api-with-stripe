2df4f8ce059d4c8fb004f615b90a185f275216ef
Adeonir Kohl <adeonir@gmail.com>
2024-03-16 20:24:03 -0300

    Create stripe function to handle checkout session

--- src/lib/stripe.ts
+++ src/lib/stripe.ts
@@ -12,3 +12,25 @@ const config = {
 export const stripe = new Stripe(config.stripe.secretKey, {
   apiVersion: '2023-10-16',
 })
+
+export const handleCheckoutSession = async (userId: string) => {
+  try {
+    const session = await stripe.checkout.sessions.create({
+      payment_method_types: ['card'],
+      mode: 'subscription',
+      success_url: `${env.APP_URL}/success`,
+      cancel_url: `${env.APP_URL}/cancel`,
+      client_reference_id: userId,
+      line_items: [
+        {
+          price: env.PRO_PLAN_PRICE_ID,
+          quantity: 1,
+        },
+      ],
+    })
+
+    return { url: session.url }
+  } catch (error) {
+    console.error('Error creating checkout session', error)
+  }
+}
