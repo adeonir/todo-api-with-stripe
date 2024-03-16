5aeb2c1b0bbb7303f0d4295c5ec95a8e74d5c2ce
Adeonir Kohl <adeonir@gmail.com>
2024-03-16 12:46:07 -0300

    Install and setup eslint with prettier

---
+++ .eslintrc.cjs
@@ -0,0 +1,96 @@
+const { resolve } = require('node:path');
+
+const project = resolve(process.cwd(), 'tsconfig.json');
+
+/** @type {import('@types/eslint').Linter.BaseConfig} */
+module.exports = {
+  root: true,
+  env: {
+    es2021: true,
+    node: true,
+  },
+  extends: [
+    ...[
+      '@vercel/style-guide/eslint/node',
+      '@vercel/style-guide/eslint/typescript',
+    ].map(require.resolve),
+    'eslint:recommended',
+    'plugin:prettier/recommended',
+  ],
+  parser: '@typescript-eslint/parser',
+  parserOptions: {
+    project,
+    ecmaVersion: 'latest',
+    sourceType: 'module',
+  },
+  settings: {
+    'import/resolver': {
+      typescript: {
+        project,
+      },
+      node: {
+        extensions: ['cjs', '.mjs', '.js', '.jsx', '.ts', '.tsx'],
+      },
+    },
+  },
+  ignorePatterns: [
+    'node_modules/',
+    'dist/',
+    '.eslintrc.cjs',
+  ],
+  plugins: [
+    'prettier',
+    'simple-import-sort',
+  ],
+  rules: {
+    'prettier/prettier': [
+      'error',
+      {
+        semi: false,
+        printWidth: 120,
+        singleQuote: true,
+        trailingComma: 'all',
+        arrowParens: 'always',
+      },
+    ],
+    '@typescript-eslint/consistent-type-imports': 'error',
+    '@typescript-eslint/explicit-function-return-type': 'off',
+    '@typescript-eslint/explicit-module-boundary-types': 'off',
+    '@typescript-eslint/no-floating-promises': 'off',
+    '@typescript-eslint/no-misused-promises': 'off',
+    '@typescript-eslint/no-unsafe-assignment': 'off',
+    '@typescript-eslint/no-unsafe-call': 'off',
+    '@typescript-eslint/no-unsafe-member-access': 'off',
+    'eslint-comments/require-description': 'off',
+    'import/consistent-type-specifier-style': ['error', 'prefer-top-level'],
+    'import/named': 'off',
+    'import/newline-after-import': 'error',
+    'import/no-duplicates': 'error',
+    'import/no-extraneous-dependencies': 'off',
+    'import/no-named-as-default-member': 'off',
+    'import/first': 'error',
+    'import/order': 'off',
+    'simple-import-sort/imports': 'error',
+    'simple-import-sort/exports': 'error',
+    'padding-line-between-statements': [
+      'error',
+      { blankLine: 'always', prev: 'if', next: '*' },
+      { blankLine: 'always', prev: '*', next: 'return' },
+      { blankLine: 'always', prev: '*', next: 'function' },
+      { blankLine: 'always', prev: 'function', next: '*' },
+      { blankLine: 'always', prev: ['const', 'let', 'var'], next: '*' },
+      {
+        blankLine: 'any',
+        prev: ['const', 'let', 'var'],
+        next: ['const', 'let', 'var'],
+      },
+      { blankLine: 'always', prev: 'directive', next: '*' },
+    ],
+    'no-console': [
+      'warn',
+      {
+        allow: ['info', 'warn', 'error'],
+      },
+    ],
+  },
+};
